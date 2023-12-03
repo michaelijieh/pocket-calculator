5df369e48bc3e19f9ea32d09f9463167c4419573
michaelijieh <michaelijieh@gmail.com>
2023-12-03 03:32:33 +0100

    input operations

--- js-file.js
+++ js-file.js
@@ -1,94 +1,93 @@
-let figures = "";
-
 const buttons = document.querySelectorAll('.numButton');
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        
-        let info = button.value
-        if (info === "ac".toUpperCase()) {
-            figures = 0
-            displayResult(figures)
-        
-        } else if (info === "%") {
-            percentage(figures)
+        let input = button.value
+        inputCollector(input)
 
-        } else if (info == "c".toUpperCase()) {
-            if (figures == "0" || 0) {
-                figures = 0
-                displayResult(figures)
-            } else {
-                if (figures.length == 1) {
-                    figures = 0
-                    displayResult(figures);
-                } else {
-                    figures = figures.slice(0, -1);
-                    displayResult(figures)
-                }
-
-            }
-
-
-        } else {
-            if (figures[0] === "0") {
-                figures = figures.slice(1)
-                displayResult(figures)
-            }
-            figures += info
-            if (figures.length >= 15) {
-                displayErrorMsg();
-            } else if (figures.length <= 15) {
-                displayResult(figures)
-            } else {
-                displayResultFloat(figures)
-            }
-
-        }
-         
     })
 })
 
+let inputs = "";
 
 
-// function reset () {
-//     const response = document.getElementById('buttonAC');
-//     response.addEventListener('click', () => {
-//         location.reload()
-//     })
-// }
+function inputCollector(input) {
+    let numbers = ["0","1","2","3","4","5","6","7","8","9"]
 
-// reset ()
+    if (input == "AC") {
+        inputs = "0";
+        zeroInputs()
+        displayInput(inputs)
 
-function basicMathOps (info) {
-    if (info === "+") {
-        sum()
-    } else if (info === "-") {
-        substract()
-    } else if (info === "*") {
-        multiply()
-    } else {
-        divide ()
+    } else if (input == "." && (!inputs.includes("."))) {
+        if (inputs == "0" || 0 || "") {
+            inputs = "0.";
+        displayInput(inputs)
+        } else if (inputs == "") {
+            inputs = '0.'
+            displayInput(inputs)
+        }
+        else {
+            inputs += ".";
+            displayInput(inputs)
+        }
+    } else if (input == "C") {
+        if (inputs == "0" || 0 || "") {
+            inputs = 0;
+            zeroInputs()
+            displayInput(inputs) 
+        } else if ((inputs.length == 1) || inputs == "." || (parseFloat(inputs) <= 0.1 && (inputs.includes(".")) && inputs.length == 3) || inputs == "0") {
+            inputs = 0;
+            zeroInputs()
+            displayInput(inputs)
+        } else if (inputs == "0" || 0) {
+            zeroInputs()
+            displayInput(inputs)
+        }
+        else {
+            inputs = inputs.slice(0, -1)
+            displayInput(inputs)
+        }
+    } else if (numbers.includes(input)) {
+        if (input == "0" && inputs.length == 1 && inputs.startsWith("0" || "0.")) {
+            inputs = "0";
+            displayInput(inputs);
+
+        } else if (inputs.startsWith("0" || "0.") && parseFloat(inputs) == 0) {
+            inputs += input;
+            inputs = inputs.slice(1);
+            displayInput(inputs);
+        } else if (!numbers.includes("0") && inputs == "0" || "0.") {
+            inputs += input;
+            displayInput(inputs)
+
+        } 
+        else {
+            inputs += input;
+            displayInput(inputs);
+        }
+    
+    
+    
+    } else if (input == "%") {
+        percentage(inputs)
+    } else if (input == "=") {
+        equalsTo(inputs)
     }
 }
 
-function resultMathOps (info ) {
-    if (info === "%") {
-        percentage()
-    } else {
-        
+
+function displayInput (inputs) {
+    if (inputs.length > 15) {
+        zeroInputs ()
+        return document.querySelector('.outputText').innerText = "Error"
     }
+    return document.querySelector('.outputText').innerText = inputs;
 }
 
-function displayResult (message) {
-    return document.querySelector('.outputText').innerText = parseFloat(message);
-}
-
-function displayResultFloat (message) {
-    return document.querySelector('.outputText').innerText = message;
-}
-
-function displayErrorMsg () {
-
-    return document.querySelector('.outputText').innerText = "Err. Max Exc.";
+function zeroInputs () {
+    inputs = "0";
+    console.log("no")
+    return inputs
 }
 
 function sum (a, b) {
@@ -107,12 +106,82 @@ function divide (a, b) {
     return a / b;
 }
 
-function percentage (figures) {
-    figures = parseFloat(figures)
-    figures = figures / 100
-    return displayResult(figures)
+function percentage (inputs) {
+    inputs = parseFloat(inputs)
+    inputs = inputs / 100
+    
+    displayInput(inputs)
+    zeroInputs()
+    inputs += inputs
+    return String(inputs)
 }
 
-function equalsTo (a) {
-    return a
+function equalsTo (inputs) {
+    inputs = parseFloat(inputs)
+    
+    displayInput(inputs)
+    zeroInputs()
+    return inputs
 }
+
+
+
+
+
+
+
+// let info = button.value
+// if (info == "ac".toUpperCase()) {
+//     figures = 0
+//     displayResult(figures)
+
+// } else if (info == "%") {
+//     percentage(figures)
+
+// } else if (info == ".") {
+//     figures += "."
+//     displayResultFloat(figures)
+    
+// }
+// else if (info == "c".toUpperCase()) {
+//     if (figures == "0" || 0) {
+//         figures = 0
+//         displayResult(figures)
+//     } else {
+//         if (figures.length == 1) {
+//             figures = 0
+//             displayResult(figures);
+//         } else {
+//             figures = figures.slice(0, -1);
+//             displayResult(figures)
+//         }
+
+//     }
+
+
+// } else {
+//     if (figures[0] === "0") {
+//         figures = figures.slice(1)
+//         displayResult(figures)
+//     }
+//     figures += info
+//     if (figures.length >= 15) {
+//         displayErrorMsg();
+//     } else if (figures.length <= 15) {
+//         displayResult(figures)
+//     } else {
+//         displayResultFloat(figures)
+//     }
+
+// }
+ 
+
+
+// function reset () {
+//     const response = document.getElementById('buttonAC');
+//     response.addEventListener('click', () => {
+//         location.reload()
+//     })
+// }
+
+// reset ()
